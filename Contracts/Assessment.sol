aa664d03ce127392790c627cab99f5b8121c856f
Gaurav Sharma <gaurav12345sharma1847@gmail.com>
2024-07-04 14:51:38 +0530

    Update Assessment.sol

--- Contracts/Assessment.sol
+++ Contracts/Assessment.sol
@@ -1,72 +1,60 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
-    address payable public owner;
-    uint256 public balance;
+    address payable private owner;
+    mapping(address => uint256) private balances;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    event Deposit(address indexed account, uint256 amount);
+    event Withdraw(address indexed account, uint256 amount);
     event Transfer(address indexed from, address indexed to, uint256 amount);
 
-    constructor(uint initBalance) payable {
+    constructor() payable {
         owner = payable(msg.sender);
-        balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
+    function getBalance() public view returns (uint256) {
+        return balances[msg.sender];
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+    function getOwner() public view returns (address) {
+        return owner;
     }
 
-    // custom error
+    function deposit() public payable {
+        uint256 _amount = msg.value;
+        uint256 _previousBalance = balances[msg.sender];
+        balances[msg.sender] += _amount;
+        assert(balances[msg.sender] == _previousBalance + _amount);
+        emit Deposit(msg.sender, _amount);
+    }
+
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
+    
+    function withdraw(uint256 _amount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
+        uint256 _previousBalance = balances[msg.sender];
+        if (balances[msg.sender] < _amount) {
             revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
+                balance: balances[msg.sender],
+                withdrawAmount: _amount
             });
         }
-
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+        balances[msg.sender] -= _amount;
+        payable(msg.sender).transfer(_amount);
+        assert(balances[msg.sender] == (_previousBalance - _amount));
+        emit Withdraw(msg.sender, _amount);
     }
 
-    function transfer(address payable _to, uint256 _amount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        require(balance >= _amount, "Insufficient balance");
-        uint _previousBalance = balance;
-        balance -= _amount;
-        (bool success, ) = _to.call{value: _amount}("");
-        require(success, "Transfer failed");
-        assert(balance == (_previousBalance - _amount));
+    function transfer(address _to, uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        require(_to != address(0), "Invalid address");
+        uint256 _previousBalanceFrom = balances[msg.sender];
+        uint256 _previousBalanceTo = balances[_to];
+        balances[msg.sender] -= _amount;
+        balances[_to] += _amount;
+        assert(balances[msg.sender] == _previousBalanceFrom - _amount);
+        assert(balances[_to] == _previousBalanceTo + _amount);
         emit Transfer(msg.sender, _to, _amount);
     }
 }
